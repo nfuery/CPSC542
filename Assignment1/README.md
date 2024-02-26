7da0ad7cf08ff2d924465148dfabcce92962ec17
Noah Fuery <nfuery@chapman.edu>
2024-02-25 22:34:24 -0800

    Finalizing RandomForest.py and adding report

---
+++ Assignment1/README.md
@@ -0,0 +1,29 @@
+ReadMe - Student Info
+Includes:
+- Any resources you used to complete the task.
+- Link to any data if not uploaded to canvas
+- A link to your code repo in github
+
+## Information
+
+* Name: Noah Fuery
+* Student ID: 2377792
+* Course: CPSC 542 - Deep Learning and Computer Vision
+* Date: Spring 2024
+* Assignment: Database
+
+## In-class Collaborators
+
+* Shanzeh Bandukda
+* Ponthea Zahraii
+
+## Resources
+
+* https://www.tensorflow.org/tutorials/images/cnn
+* https://www.tensorflow.org/tutorials/images/classification
+* https://keras.io/keras_cv/
+* https://nbdev.fast.ai/tutorials/qmd_intro.html
+
+## Citations
+
+* Dataset cited from: https://www.cs.toronto.edu/~kriz/learning-features-2009-TR.pdf
--- Assignment1/RandomForest.py
+++ Assignment1/RandomForest.py
@@ -1,39 +1,29 @@
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from keras.datasets import cifar10
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.datasets import cifar10
 
 # Load CIFAR-10 data
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-# Reshaping the data
-x_train = x_train.reshape(x_train.shape[0], x_train.shape[1] * x_train.shape[2] * x_train.shape[3])
-x_test = x_test.reshape((x_test.shape[0], x_train.shape[1] * x_train.shape[2] * x_train.shape[3]))
+# Preprocess the data
+x_train = x_train.reshape((x_train.shape[0], -1)) / 255.0
+x_test = x_test.reshape((x_test.shape[0], -1)) / 255.0
 
-# Normalizing pixel values
-x_train /= 255
-y_train /= 255
-
-# Splitting first 1000 samples to validation and rest into training
-x_test = x_train[:1000]
-x_train = x_train[1000:]
-
-y_test = y_train[:1000]
-y_train = y_train[1000:]
-
-# Define the RandomForestClassifier with custom hyperparameters
-clf = RandomForestClassifier(n_estimators=10, random_state=42)
+# Split the dataset
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2)
 
 # Train the random forest classifier
+clf = RandomForestClassifier(n_estimators=100)
 clf.fit(x_train, y_train)
 
-# Evaluate the classifier on the validation set
-y_train_pred = clf.predict(x_train)
-y_test_pred = clf.predict(x_test)
-
-accuracy = accuracy_score(y_train, y_train_pred)
+# Evaluate the classifier
+y_pred = clf.predict(x_val)
+accuracy = accuracy_score(y_val, y_pred)
 print(f"Validation Accuracy: {accuracy}")
 
 # Test on the testing set
+y_test_pred = clf.predict(x_test)
 test_accuracy = accuracy_score(y_test, y_test_pred)
 print(f"Test Accuracy: {test_accuracy}")
